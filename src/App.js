fc4b813bb1eb98feac5bfed321977677ddfe29e5
Jhon Hamilton Franco <hamilton5217@gmail.com>
2022-09-10 00:07:48 -0500

    add account detection and change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const handleClick = () => {
-    window.ethereum.on('accountsChanged', function (accounts) {
-      // Time to reload your interface with accounts[0]!
-      console.log("info de cuenta", accounts)
+  const [account, setAccount] = useState(null);
+  const handleClick = async () => {
+    console.log("entro a click");
+    window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((accounts) => {
+        setAccount(accounts);
+      });
+    window.ethereum.on("accountsChanged", function (accounts) {
+      console.log("cambio cuenta", accounts);
+    });
+
+    window.ethereum.on("chainChanged", function (chain) {
+      console.log("cambio cambio la cadena", chain);
     });
   };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {account ? (
+          <p>account: {account}</p>
+        ) : (
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
